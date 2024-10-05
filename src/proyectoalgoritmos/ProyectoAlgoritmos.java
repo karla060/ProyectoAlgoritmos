b1228eb9d6a854cdff5ce08e249de5560527d011
karla060 <kpirirg1@miumg.edu.gt>
2024-10-05 08:58:32 -0600

    Primera version del proyecto

--- src/proyectoalgoritmos/ProyectoAlgoritmos.java
+++ src/proyectoalgoritmos/ProyectoAlgoritmos.java
@@ -14,15 +14,20 @@ public class ProyectoAlgoritmos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
-    
+  
     Scanner scan = new Scanner(System.in);
-      
-      
+    
+    
+    
       
       //Mostrar mensaje indicando la seccion de categorías 
+      //1.1.	Definición de Categorías:
+                    
+    /*public class seccionCategorias {
+    public void Categorias(Scanner scan) {*/
+        
+               
        System.out.println("  ---Seccion de Categorias de Productos---  "); 
-     
       
        
        
@@ -74,13 +79,15 @@ public class ProyectoAlgoritmos {
        
        System.out.println("Programa terminado ");
     
-       
-       /*
-       private static void gestionarCaracteristicas(Scanner scanner, GestionCaracteristicas gestionCaracteristicas) {  
-       
-       */
+    
        
        
+       // 1.2.	Definición de Características:
+    /*public class seccionCaracteristicas {
+    public void Características (Scanner scan){*/
+       
+    
+
       System.out.println(" ---Seccion de Caracteristicas de Productos--- ");
       
        int respuesta2 = 0; 
@@ -127,8 +134,15 @@ public class ProyectoAlgoritmos {
        
        System.out.println("Programa terminado ");
        
+
        
        
+       // 1.3.	Definición de Especificaciones:
+    
+   /* public class seccionEspecificaciones {
+    public void Especificaciones ( Scanner scan) {*/
+       
+
        System.out.println(" ---Seccion de Especificaciones de los Productos--- ");
       
        int respuesta3 = 0; 
@@ -175,12 +189,26 @@ public class ProyectoAlgoritmos {
        } while (respuesta3 ==1 || respuesta3 == 2 || respuesta3 ==3);
        
        System.out.println("Programa terminado ");
+       scan.close();
+       
+        }
+}
+
+     /*   1.4 Asignación de Categorías, Características y Especificaciones a Productos
+       System.out.println("---	Asignación de Categorías, Características y Especificaciones a Productos ---");
+       
+       System.out.print("Selecciona la categoría a la que pertenece el producto: ");
+       
+       System.out.print("Ingrese los valores para la característica/especificación");
        
        
-       System.out.println("");
-               
+       //1.5.	Alta de Productos:
        
-    }       
+       System.out.print("Selecciona la categoría a la que pertenece el producto: ");
+       
+       
+    }  
+    }  */     
     
-}   
    
+
